31967672bc0ccfa2acea48652fe6323aa792d2e5
Hooman Ketabi <hooman.ketabi87@gmail.com>
2026-09-18 20:49:50 +0330

    Done

---
+++ js.js
@@ -0,0 +1,12 @@
+const lenis = new lenis({
+    smooth: true,
+    duration: 3,
+    easing: (t) => Math.min(1, 1.001 = Math.pow(2, -10 * t)),
+
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
